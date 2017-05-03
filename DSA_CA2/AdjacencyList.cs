cce61af58f6026616568f1f7eb21e4751f17186f
JasonMcClafferty <jasoncathalmcclafferty@gmail.com>
2017-05-04 00:45:15 +0100

    Worked on menu interface and AdjacencyList class optimisation.

---
+++ DSA_CA2/AdjacencyList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSA_CA2
+{
+
+   class AdjacencyList
+    {
+        List<int>[] verteces;
+        Dictionary<int, int> parent;
+        Dictionary<int, int> level;
+        public AdjacencyList (List<int>[] v)
+        {
+            this.verteces = v;
+        }
+        public void printAL(List<int> list)
+        {
+            Console.Write("\nPrinting List \nSize: ");
+            Console.WriteLine(list.Count);
+            Console.Write("Nodes: ");
+
+            foreach (int i in list)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.Write("\n");
+        }
+        public int shortestPath(int fromNode, int toNode)
+        {
+            int i = toNode;
+            string shortestPath = "";
+
+            Dictionary<int, int> parent = BFS(fromNode);
+
+            if (parent.ContainsKey(i))
+            {
+                while (i != -1)
+                {
+                    shortestPath += (i + " -> ");
+                    i = parent[i];
+                }
+                Console.Write(shortestPath);
+            }
+            else return -1;
+
+            return 0;
+        }
+        public Dictionary<int, int> BFS(int s)
+        {
+            // level is a dictionary of verteces that have been assigned a level.
+            // S is the start node, so is assigned level 0.
+            Dictionary<int, int> level = new Dictionary<int, int>();
+            level.Add(s, 0);
+
+            // Parent is a dictionary of verteces and their parent nodes.
+            // S is the start node, it has no parent.      
+            Dictionary<int, int> parent = new Dictionary<int, int>();
+            parent.Add(s, -1);
+
+            // Next is every node thats reachable in i steps.
+            List<int> next = new List<int>();
+            
+            // frontier is a list of everything reachable in i-1 steps.
+            List<int> frontier = new List<int>() {s};
+            
+            // i is an iteration counter.
+            int i = 1;
+
+            while (!(frontier.Count == 0))
+            {
+                next = new List<int>();
+                foreach (int u in frontier)
+                    foreach (int v in verteces[u])
+
+                    {
+                        // If int v is not in levels   
+                        if (!level.ContainsKey(v))
+                        {
+                            level.Add(v, i);
+                            parent.Add(v, u);
+                            next.Add(v);
+                        }
+                    }
+                frontier = next;
+                i++;
+                // printAL(frontier);
+            }
+            return parent;
+        }
+        public int getLength()
+        {
+            return verteces.Length;
+        }
+        public Dictionary<int, int> getParents()
+        {
+            return parent;
+        }
+        public Dictionary<int, int> getLevels()
+        {
+            return level;
+        }
+
+        public List<int> getNeighbours(int i)
+        {
+            return verteces[i];
+        }
+    }
+}
---
+++ DSA_CA2/Menu.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_CA2
+{
+    class Menu
+    {
+        private static string instructions = "Please Enter:\n[1] Enter Graph Data\n[2] Shortest Path\n[3] Print List\n[0] Exit";
+        private AdjacencyList adjList;
+
+        public Menu(AdjacencyList aList)
+        {
+            this.adjList = aList;
+        }
+
+        public void printList()
+        {
+            for (int i = 0; i < adjList.getLength(); i++)
+            {
+                if (adjList.getNeighbours(i).Count == 0)
+                {
+                    continue;
+                }
+
+                String neighbours = "";
+                foreach (int j in adjList.getNeighbours(i))
+                {
+                    neighbours += j + " ";
+                }
+                Console.WriteLine("Node " + (i + 1) + "\nNeighbours: " + neighbours + "\n\n");
+            }
+        }
+
+        public void menuLoop()
+        {
+            int choice = -1;
+
+            while(choice != 0)
+            {
+                Console.WriteLine(instructions);
+                choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    case 1:
+                        Console.WriteLine("createAdjacencyList();");
+                        break;
+                    case 2:
+                        this.launchShortestPath();
+                        break;
+                    case 3:
+                        this.printList();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid choice");
+                        break;
+                }
+            }
+        }
+
+        // Reads nodes, checks error code and handles user request for shortest path
+        public void launchShortestPath()
+        {
+            int check;
+            do
+            {
+                Console.WriteLine("Which node would like to start from? ");
+                int startNode = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nWhich node would you like the shortest path to? ");
+                int endNode = Int32.Parse(Console.ReadLine());
+
+                check = adjList.shortestPath(startNode, endNode);
+
+                if (check == -1)
+                {
+                    Console.WriteLine(" Node(s) not found, try again.");
+                }
+
+            } while (check == -1);
+        }
+
+        public static void Main(string[] Args)
+        {
+            List<int>[] v = new List<int>[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                v[i] = new List<int>();
+            }
+
+            v[0] = new List<int>() { 7, 1 };
+            v[1] = new List<int>() { 0, 6 };
+            v[2] = new List<int>() { 3, 4, 5, 6 };
+            v[3] = new List<int>() { 2, 4, 6 };
+            v[4] = new List<int>() { 2, 3, 5 };
+            v[5] = new List<int>() { 2, 4 };
+            v[6] = new List<int>() { 1, 2, 3 };
+            v[7] = new List<int>() { 0 };
+
+            AdjacencyList adj = new AdjacencyList(v);
+
+            Menu menu = new Menu(adj);
+            menu.menuLoop();
+            menu.printList();
+
+        }
+    }
+}
